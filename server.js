62ce3823f1630d6eac5a5da23400386b004b267e
Monesa <mthogulu@uncc.edu>
2020-09-29 11:43:41 -0400

    loading data from json file

--- server.js
+++ server.js
@@ -6,51 +6,14 @@ const port = 3000
 
 app.use('/',express.static('public'));
 
-const budget = {
+const fs = require('fs');
 
-    myBudget : [
-
-        {
-            title:'Restaurant',
-            budget: 50,
-            color: '#FF5733'
-        },
-        {
-            title:'Groceries',
-            budget: 100,
-            color: '#DDDF39'
-        },
-        {
-            title:'Rent',
-            budget: 400,
-            color: '#84DF39'
-        },
-        {
-            title:'Travel',
-            budget: 500,
-            color: '#39E0C2'
-        },
-        {
-            title:'Utilities',
-            budget: 300,
-            color: '#3F7DEA'
-        },
-        {
-            title:'Shopping',
-            budget: 300,
-            color: '#C354F0'
-        },
-        {
-            title:'Others',
-            budget: 100,
-            color: '#000000'
-        }
-    ]
-
-}
 
 app.get('/budget',(req, res) => {
-    res.json(budget);
+    fs.readFile('dataset.json', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
 });
 
 app.get('/hello', (req, res) => {
